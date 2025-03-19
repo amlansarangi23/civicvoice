2b43493f354b1df94759300a34bde11a41b51ca4
Amlan Sarangi <amlan.sarangi23@gmail.com>
2025-03-19 15:49:07 +0530

    Update page.tsx

--- app/issue/createissue/page.tsx
+++ app/issue/createissue/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import React, { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import axios from "axios";
